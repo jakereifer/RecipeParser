8380fad46dc71832b5c6aa86a678bcd0dcc0e1c4
estettin <elanastettin@gmail.com>
2019-02-25 13:55:07 -0600

    add recipe class and ojbect

--- helpers.py
+++ helpers.py
@@ -6,3 +6,23 @@ def findWordsInSteps(keywords, strings):
 			if keyword in string.lower():
 				keywordsfound.append(keyword.capitalize())
 	return keywordsfound
+
+
+class Recipe(object):
+	name = ""
+	ingredients = []
+	steps = []
+	p_methods = []
+	s_methods = []
+	tools = []
+	tags = []
+
+
+	def __init__(self, name="", ingredients=[], steps=[], p_methods=[], s_methods=[], tools=[], tags=[]):
+		self.name = name
+		self.ingredients = ingredients
+		self.steps = steps
+		self.p_methods = p_methods
+		self.s_methods = s_methods
+		self.tools = tools
+		self.tags = tags
